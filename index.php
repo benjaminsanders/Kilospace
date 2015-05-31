7f431d1f4f7ce2b1cef31c35dc1a68e153677b8b
Benjamin Sanders <Contact@benjaminsanders.net>
2015-05-31 08:48:44 -0600

    Pre-Alpha 0.0.2
    General update

--- index.php
+++ index.php
@@ -1,26 +1,59 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="../assets/css/style.css"/>
-
-<title>Kilospace</title>
-</head>
+<link href="../assets/stylesheet.css" rel="stylesheet">
 <body>
 
-<p>
-  <retype>The intergalactic space agency is testing some things right now.
-    Please contact us later.</retype>
-</p>
-<a id="mtoogle" style="cursor:pointer;">
-<img src="play.png"></a>
-<audio id="playTune" autoplay loop>
-<source src="loop.mp3">
-</audio>
-<div id="div_with_text"><a href="/">Contact</a>
-  
-</div>  
-</body>
-<script type="text/javascript" src="../assets/js/jquery-1.3.2.min.js"></script>
-<script type="text/javascript" src="../assets/js/general.js"></script>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <!-- Begin page content -->
+    <div class="container">
+    <center><iframe src="https://kilo.space/home.php" width="900" height="500" frameBorder="0"></iframe></center>
+      </div>
+
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted pull-left">Copyright &copy; Green Ridge Ltd. 2013 - 2015</span>
+        <span class="text-muted pull-right">contact@kilo.space</span>
+      </div>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../assets/js.js"></script>
+  </body>
 </html>
---
+++ views/not_logged_in.php
@@ -0,0 +1,39 @@
+<body>
+
+    <div class="container">
+<?php
+// show potential errors / feedback (from login object)
+if (isset($login)) {
+    if ($login->errors) {
+        foreach ($login->errors as $error) {
+            echo $error;
+        }
+    }
+    if ($login->messages) {
+        foreach ($login->messages as $message) {
+            echo $message;
+        }
+    }
+}
+?>
+
+<!-- login form box -->
+      <form class="form-signin" method="post" action="index.php" name="loginform">
+        <h2 class="form-signin-heading">Sign In</h2>
+        <label for="login_input_username" class="sr-only">Username</label>
+        <input type="email" id="login_input_username"  class="form-control" type="text" name="user_name" placeholder="Username" required autofocus />
+        
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input id="inputPassword" class="form-control" type="password" name="user_password" placeholder="Password" autocomplete="off" required />
+        
+        <input class="btn btn-lg btn-primary pull-left" type="submit"  name="login" value="Log In" />
+        <a class="btn btn-lg btn-info pull-right" href="register.php">Register</a>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
