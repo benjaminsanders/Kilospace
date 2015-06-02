42580d88d52211753c85805de5b69729e49e957c
Benjamin Sanders <Contact@benjaminsanders.net>
2015-06-02 08:27:18 -0600

    Pre-Alpha 0.0.6
    Small alterations

--- Kilospace/assets/views/home.php
+++ Kilospace/assets/views/home.php
@@ -1,13 +1,52 @@
+<style>.skills{background-image:url(../data/img/skills/skills.png);background-repeat:no-repeat;display:block;}</style>
 <div class="container">
 <div class="row">
 <!-- Large Left Area -->
-<div class="col-lg-8 col-sm-12">
+<div class="col-lg-8 col-sm-12" style="height:580px">
 <img src="../assets/img/bg1.png" class="img-responsive img-thumbnail"/> </div>
 <!-- Large Left Area -->
 <div class="col-lg-4 col-sm-12">
   <p id="fade1"><img src="../assets/img/bg3.png"class="img-responsive img-thumbnail"/> </p>
   <p id="fade2"><img src="../assets/img/bg4.png" alt=""class="img-responsive img-thumbnail"/></p>
   <p id="fade3"><img src="../assets/img/bg2.png" alt="" class="img-responsive img-thumbnail"/></p>
+</div>
+</div>
+<hr />
+<div class="row well">
+<div class="col-lg-4 col-sm-12" style="height:600px">
+<a class="twitter-timeline" href="https://twitter.com/kilospace" data-widget-id="605392934029000705">Tweets by @kilospace</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
+<div class="col-lg-1 col-sm-12">
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        jQuery('[rel=popover]').popover();
+    });
+</script>
+
+
+<h5>Skills</h5>
+<a title="Communication" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill improves the staff members ability to operate communication equipment.</font></div>"><i class="skills skills-communication"></i></a>
+
+<a title="Construction" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill lets staff members build rooms and equipment throughout the base.</font></div>"><i class="skills skills-construction"></i></a>
+
+<a title="Farming" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill lets staff plant seeds, care for crops, and harvest the resulting food.</font></div>"><i class="skills skills-farming"></i></a>
+
+<a title="Financial" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill ensures that the staff member can efficiently handle funds.</font></div>"><i class="skills skills-finance"></i></a>
+
+<a title="Health" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill is required for staff members to perform medical care on other staff.</font></div>"><i class="skills skills-health"></i></a>
+
+<a title="Management" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill allows the staff member to better assist with management duties.</font></div>"><i class="skills skills-management"></i></a>
+
+<a title="Mining" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill allows staff to mine raw ore and process it into usable material.</font></div>"><i class="skills skills-mining"></i></a>
+
+<a title="Psychology" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill is needed for staff to counsel and prescribe medication to other staff.</font></div>"><i class="skills skills-psychology"></i></a>
+
+<a title="Repair" rel="popover" data-placement="bottom" data-html=true data-trigger="hover" data-content="<div style='width:200px'><font size='1px'>This skill is required for staff to perform repairs to the base and equipment.</font></div>"><i class="skills skills-repair"></i></a>
+</div>
+<div class="col-lg-7 col-sm-12 label label-default">
+<h2>Updates</h2>
+<p>This is where updates will be posted.</p>
 </div>
 </div>
 </div>
